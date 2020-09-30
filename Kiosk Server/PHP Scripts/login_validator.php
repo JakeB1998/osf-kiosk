111e68cae3668d1d5ccd75d22c9f5b95f680556c
Botka.Jake <jake.m.botka@gmail.com>
2020-09-30 18:38:27 -0500

    New

--- Kiosk Server/PHP Scripts/login_validator.php
+++ Kiosk Server/PHP Scripts/login_validator.php
@@ -1,22 +1,5 @@
 <?php
 $username = 'fname';
-<<<<<<< HEAD
-$pwd = "fpwd";
-$email = "femail";
-session_start();
-if (isset($_POST($username))) {
-    $client_username = $_POST($username);
-    echo $client_username;
-}
-if (isset($_POST($pwd))) {
-    $client_password = $_POST($pwd);
-    echo $client_password;
-}
-if (isset($_POST($email))) {
-    $client_email = $_POST($email);
-    echo $client_email;
-}
-=======
 $pwd = 'fpwd';
 $email = 'femail';
 $client_username = $_POST[$username];
@@ -36,5 +19,4 @@ if (isset($client_email)) {
 
 header("Location: ../../Kiosk Program/src/main/index.html");
 die();
->>>>>>> db274908a92f596f3e56fea4d9e78808121c43db
 ?>
