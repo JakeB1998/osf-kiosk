381ff3caf24d86761476b6656ecb569e84d49940
greg <gjschwa@ilstu.edu>
2020-09-30 18:28:53 -0500

    Big boi commit

--- Kiosk Server/PHP Scripts/login_validator.php
+++ Kiosk Server/PHP Scripts/login_validator.php
@@ -1,19 +1,22 @@
 <?php
-$username = "fname";
-$pwd = "fpwd";
-$email = "femail";
-$client_username = $_POST($username);
+$username = 'fname';
+$pwd = 'fpwd';
+$email = 'femail';
+$client_username = $_POST[$username];
 session_start();
-if (isset($client_username) {
+if (isset($client_username)) {
     
     echo $client_username;
 }
-if (isset($_POST($pwd))) {
-    $client_password = $_POST($pwd);
+$client_password = $_POST[$pwd];
+if (isset($client_password)) {
     echo $client_password;
 }
-if (isset($_POST($email))) {
-    $client_email = $_POST($email);
+$client_email = $_POST[$email];
+if (isset($client_email)) {
     echo $client_email;
 }
+
+header("Location: ../../Kiosk Program/src/main/index.html");
+die();
 ?>
