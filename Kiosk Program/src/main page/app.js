d17a36d6d8d27c211fd23d31d5b27a064f8e91ea
JakeB1998 <49763318+JakeB1998@users.noreply.github.com>
2020-10-03 14:12:33 -0500

    Botka dev (#15)
    * First commit
    
    * Test
    
    * Added entry point html file for kiosk interface program
    
    * Fixed file ext
    
    * Added the flow of the html document
    
    * Added cryptography lib
    
    * Added cryptography lib
    
    * added secure data object as well as hashing function from the crpytojs lib
    
    * Added classes to store server information and functions to connect to the webserver
    
    * Added classes to store server information and functions to connect to the webserver
    
    * base
    
    * Wrapping up server communication base classes
    
    * Added comments and some hot fixes
    
    * Added some php files to the Server folder
    
    * a
    
    * Fixed
    
    * A
    
    * New
    
    * Added function objects to parse information about plugged in apps
    
    * Fixed formatting problem and added meta data to html
    
    * Added url parser script to parse parameters tagged onto the url. Additionally, I added php code to attack paramaters to the url before deploying to browser client
    
    * Added a parse function to parse login information

---
+++ Kiosk Program/src/main page/app.js
@@ -0,0 +1,90 @@
+
+
+/**
+ * Called when the window is fully loaded
+ */
+window.addEventListener('load', (event) =>
+{
+    let params = getURLParams();
+    logParamaters(params);
+    let loginInfo = retrieveLogInInformation(params);
+    console.log(loginInfo);
+
+    initButtons();
+    pluginAppsOnLoad = pluginAppsLoadedCallback;
+    loadApps();
+    require(['crypto-js'], function (CryptoJS) {
+    console.log("window fully loaded");
+    console.log(CryptoJS);
+    defaultCipherSettings = {
+        mode: CryptoJS.mode.CFB,
+        padding: CryptoJS.pad.AnsiX923
+    }
+    var q = new Cryptography(CryptoJS);
+    q.test("I like apples", "dhfubfskjdbkbk");
+    
+    let secureData = new SecureData(CryptoJS, 'jmbotka', "adom");
+    console.log('decrypted data: ' + secureData.getData().toString()
+                + '\nHashed data: ' + secureData.getHashDigest());
+    return CryptoJS;
+    });
+});
+
+function getURLParams(){
+    return hasParameters(document.URL) === true ? parseParameters(document.URL) : null;
+}
+
+/**
+ * Retrieves login information
+ * @param {} params 
+ */
+function retrieveLogInInformation(params = null){
+    let username = null;
+    let password = null;
+    if (params !== null){
+        username = params.find((element) => element['key'] === 'username');
+        password = params.find((element) => element['key'] === 'password');
+        if (username !== undefined && password !== undefined){
+            username = username['value'];
+            password = password['value'];
+            return {username,password};
+        }
+        console.log(username + ',' + password);
+    }
+
+    return null;
+}
+/**
+ * Test callback for http request completed state change
+ */
+  function callback(){
+    if (this.readyState == 4 && this.status == 200) {
+        //console.log(this.responseText);
+       }
+  }
+
+  /**
+   * Checks if client is logged into account.
+   */
+  function checkIfLoggedIn(){
+      // do a post with current logg in
+  }
+
+  /**
+   * Callback
+   * @param {*} apps Apps object
+   */
+  function pluginAppsLoadedCallback(apps) {
+    console.log('Apps loaded and ready: ' + apps);
+  }
+
+  /**
+   * Callback
+   * @param {*} appInfo AppInfo object
+   */
+  function pluginAppInfoLoaded(appInfo = null){
+      console.log(appInfo.toString());
+  }
+
+
+
--- Kiosk Server/PHP Scripts/login_validator.php
+++ Kiosk Server/PHP Scripts/login_validator.php
@@ -4,19 +4,64 @@ $pwd = 'fpwd';
 $email = 'femail';
 $client_username = $_POST[$username];
 session_start();
+
 if (isset($client_username)) {
     
-    echo $client_username;
+    echo 'Username: $client_username';
 }
 $client_password = $_POST[$pwd];
 if (isset($client_password)) {
-    echo $client_password;
+    echo 'Password: $client_password';
 }
 $client_email = $_POST[$email];
 if (isset($client_email)) {
-    echo $client_email;
+    echo 'Email $client_email';
 }
 
-header("Location: ../../Kiosk Program/src/main/index.html");
-die();
+
+if (is_validated($client_username, $client_password)){
+    $location = "../../Kiosk Program/src/main/index.html";
+    $paramString = addParameters([['key'=> 'username', 'value'=> $client_username],
+                                ['key'=> 'password', 'value'=> $client_password]]);
+    $location = $paramString != null ? $location + $paramString : $location;
+    header("Location: " + $location);
+    echo 'exited';
+    exit();
+}
+
+function addParameters($params = null){
+    if ($params != null){
+        $str = "/?";
+        for ($i =0; $i < sizeof($params); $i++){
+            if ($i > 0){
+                $str += "&";
+            }
+            $key = $params[$i]['key'];
+            $value =  $params[$i]['value'];
+            $str += $key + '=' + $value;
+            echo $str;
+        }
+        return $str;
+    }
+
+    return null;
+}
+
+/**
+ * Checks if is validated
+ */
+function is_validated($username = null, $password = null){
+    return validate_cred($username, $password);
+}
+
+/**
+ * Checks to see if credentials are validated
+ */
+function validate_cred($username = null, $password = null){
+    if ($username != null && $password != null){
+        //check database
+    }
+
+    return false;
+}
 ?>
