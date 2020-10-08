f1512c6e7ad997d4e942b8632fcd96c583133346
Botka.Jake <jake.m.botka@gmail.com>
2020-10-07 20:16:11 -0500

    Renamed files in accordane to conventions

--- Kiosk Server/PHP Scripts/login_validator.php
+++ Kiosk Server/PHP Scripts/login_validator.php
@@ -11,16 +11,16 @@ ob_start();
 
 if (isset($_POST[$username])) {
     $client_username = $_POST[$username];
-    echo 'username: ' . $client_username;
+    //echo 'username: ' . $client_username;
 }
 if (isset($_POST[$pwd])) {
     $client_password = $_POST[$pwd];
-    echo 'password: ' .$client_password;
+   // echo 'password: ' .$client_password;
 }
 
 if (isset($_POST[$email])) {
     $client_email = $_POST[$email];
-    echo 'email: ' . $client_email;
+    //echo 'email: ' . $client_email;
 }
 
 if(isset($_POST['fauthcode'])){
@@ -29,32 +29,27 @@ if(isset($_POST['fauthcode'])){
 
 
 if (is_validated($client_username, $client_password)){
-    echo 'validation started';
-    $location = "../../Kiosk Program/src/main page/index.html";
+    $location = "../../kiosk program/src/main page/index.html";
     $paramString = null;
     if ($client_username != null && $client_password != null){
         $paramString = addParameters([['key'=> 'authcode', 'value'=> $client_authcode]]);
     }
     $location = $paramString != null ? $location . $paramString : $location;
-    echo $location;
     header("Location: " . $location);
-    echo '<br>exited';
+    //readfile('../../kiosk program/src/main page/index.html');  //'var/www/html/OSF Project/Kiosk Program/src/main page/index.html'
     exit();
 }
 
 function addParameters($params = null){
-    echo '<br>adding params';;
     if ($params != null){
         $str = "?";
         for ($i =0; $i < sizeof($params); $i++){
-            echo $i;
             if ($i > 0){
                 $str .= "&";
             }
             $key = $params[$i]['key'];
             $value =  $params[$i]['value'];
             $str .= $key . '=' . $value;
-            echo  "<br> " . $str;
         }
         return $str;
     }
